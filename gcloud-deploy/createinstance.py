2bf0fb944797606383671cf1b90ba5d70fb281a0
nic-instruction <nicole.bade@seattlecolleges.edu>
2017-03-11 00:26:24 -0800

    Update createinstance.py

--- gcloud-deploy/createinstance.py
+++ gcloud-deploy/createinstance.py
@@ -24,7 +24,27 @@ def list_instances(compute, project, zone):
 centos_server =     open('centos_config.json', 'r').read()      # Slightly edited Rest Json with variables
 ubuntu_client =     open('ubuntu_config.json', 'r').read()      # Slightly edited Rest Json with variables
 
-def create_instance(os_type, project, zone, config_script):
+#def create_instance(compute, project, zone, name, bucket): * I"m thinking we can avoid this bucket buisness... let's see.
+def create_instance(compute, os_type, project, zone, name, config_script):
+    image_response = compute.images().getFromFamily(
+       project='centos-cloud', family='centos-8').execute()
+    source_disk_image = image_response['selfLink']
+
+    # ToDo, once OStype is selected, read the config into a new file with the name of the machine
+    # and the date-min and secs (to provide ordered uniqueness)
+    # config will be equal to the newfile
+    # I would like to make the startup script bit seperate... in fact, that's rather important.  But we'll
+    # know from the name what kind of machine this will be already... hrm.
+    
+    config =  # which will contain the specs for the centos_server or the ubuntu client.  :D
+                     # somewhere along the lines here, we need to customize the config...
+                     # AAAAAAH
+    
+    
+     return compute.instances().insert(
+     project=project,
+     zone=zone,
+     body=config).execute()
   
 
 # We'll need to read in an additional file, which is the scripting file for the server we are creating.  
